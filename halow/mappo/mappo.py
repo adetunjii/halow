12016f0484238fd30f88d763e4e23ada1bfeb393
adetunjii <adetunjithomas1@outlook.com>
2026-08-31 01:14:36 +0100

    chore: setup mappo

--- halow/mappo/mappo.py
+++ halow/mappo/mappo.py
@@ -1,11 +1,33 @@
 from mappo.actor import Actor
+from mappo.critic import Critic
+from torch.optim import Adam
+from typing import Any
 
 class Mappo:
-    def __init__(self):
-        self.drone_actor = Actor
+    def __init__(self, env: Any):
+        assert env is not None
+        self.env = env
+        self.obs_dim = self.env.observation_space.shape[0]
+        self.action_dim = self.env.action_space.shape[0]
+        
+        self._init_hyperparameters()
+        
+        self.drone_actor = Actor(self.obs_dim, self.hidden_dim, self.action_dim, self.num_layers)
+        self.rover_actor = Actor(self.obs_dim, self.hidden_dim, self.action_dim, self.num_layers)
+        self.critic = Critic(self.obs_dim, self.hidden_dim, self.num_layers)
+        
+        self.drone_actor_optim = Adam(self.drone_actor.parameters(), lr=self.learning_rate)
+        self.rover_actor_optim = Adam(self.rover_actor.parameters(), lr=self.learning_rate)
+        self.critic_optim = Adam(self.critic.parameters(), lr=self.learning_rate)
         
     def _init_hyperparameters(self):
         self.gamma = 0.95
-        self.learning_rate = 0.001
+        self.learning_rate = 0.01
         self.max_timesteps_per_episode = 200
         self.total_timesteps_per_batch = 2048
+        self.num_layers = 3
+        self.hidden_dim = 64
+        self.num_layers = 3
+    
+    def batch_experiences(self):
+        pass
